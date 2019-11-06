cc1fd7f8b60d0169e84198f2cbfa3247a30e6961
CadenaR <a01652141@itesm.mx>
2019-11-06 11:35:40 +0100

    Se añaden escenas de carga y menu inicio

--- src/game.js
+++ src/game.js
@@ -1,8 +1,9 @@
 var config = {
+    type: Phaser.AUTO,
     width: 800,
     height: 600,
-    backgroundColor: 0x000000,
-    scene: []
+    backgroundColor: '#000000',
+    scene: [LoadScene, MenuScene]
 }
 
 
