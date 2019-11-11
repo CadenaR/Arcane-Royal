e115f85b32515dee94a71e93566ff6aadfb0a98c
nesherben <nestor8299@gmail.com>
2019-11-11 18:36:55 +0100

    no message

--- src/scenes/LoadScene.js
+++ src/scenes/LoadScene.js
@@ -1,30 +1,9 @@
-
-class GameScene extends Phaser.Scene {
-
 class LoadScene extends Phaser.Scene {
-
     constructor() {
         super("loadScene");
     }
 
-
-    create() {
-
-    }
-}
-
-class LoadScene extends Phaser.Scene {
-    constructor() {
-        super("LoadScene");
-    }
-
-    preload() {
-
-    }
-
-    create() {
-        this.add.text(20, 20, "Loading game...");
-        this.scene.start("MenuScene");
+    
 
     create() {
         this.add.text(20, 20, "Loading game...");
--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -1,18 +1,10 @@
 class MenuScene extends Phaser.Scene {
     constructor() {
-
-        super("MenuScene");
-    }
-
-    create() {
-
-
         super("menuScene");
     }
 
     create() {
         
-
         const playBtn = this.add.text(this.game.renderer.width / 3, this.game.renderer.height / 3, 'Play', {
             fontSize: '100px',
             fill: '#0f0'
@@ -22,11 +14,9 @@ class MenuScene extends Phaser.Scene {
         playBtn.on('pointerover', () => {
             console.log('pointerover');
         });
-
         var that = this;
         playBtn.on('pointerdown', () => {
             this.scene.start("gameScene");
         });
-
     }
 }
