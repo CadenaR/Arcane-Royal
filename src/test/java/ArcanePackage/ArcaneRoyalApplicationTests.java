efde65e2a140b5786abb64c5c3a2179d44028403
CadenaR <a01652141@itesm.mx>
2019-12-02 00:39:11 +0100

    Proyecto Maven
    Se creó un proyecto con MAven, reorganizando la estructura del proyecto en donde ahora nuestro juego se encuentra dentro de la ruta src/main/resources/static

---
+++ src/test/java/ArcanePackage/ArcaneRoyalApplicationTests.java
@@ -0,0 +1,13 @@
+package ArcanePackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcaneRoyalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
