77aca15f529dd66a5d4f4da52df72976eb81fc41
rodron <alexsp2399@gmail.com>
2020-01-03 20:18:51 +0100

    Pruebas

--- Arcane-Royal/src/main/resources/static/WebSocket.js
+++ Arcane-Royal/src/main/resources/static/WebSocket.js
@@ -16,23 +16,25 @@ function onMessage(evt)
   datosRecib=JSON.parse(evt.data);
 
   if(datosRecib.color===player.color){
+    enemCambio=false;
     //datosRecib.sprite.mago=player.mago;
     //player.mago.sprite=datosRecib.sprite;
     
     player.mago.mAngle=datosRecib.mAngle;
     player.mago.sprite.setVelocity(datosRecib.velocityX, datosRecib.velocityY);    
-    player.mago.sprite.setPosition(datosRecib.x,datosRecib.y);
+    //player.mago.sprite.setPosition(datosRecib.x,datosRecib.y);
     if(datosRecib.anim!=undefined){
       player.mago.sprite.anims.play(datosRecib.anim+'_'+datosRecib.color, true);
     }
     
   }else{
+    enemCambio=true;
     //datosRecib.sprite.mago=player.mago.enemy;
     //player.mago.enemy.sprite=datosRecib.sprite;
     
     player.mago.enemy.mAngle=datosRecib.mAngle;
     player.mago.enemy.sprite.setVelocity(datosRecib.velocityX, datosRecib.velocityY);    
-    player.mago.enemy.sprite.setPosition(datosRecib.x,datosRecib.y);
+    //player.mago.enemy.sprite.setPosition(datosRecib.x,datosRecib.y);
     if(datosRecib.anim!=undefined){
       player.mago.enemy.sprite.anims.play(datosRecib.anim+'_'+datosRecib.color, true);
     }
