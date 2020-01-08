418dcc9a43eea5a8b422b3309a5498eb9d1b8d23
rodron <alexsp2399@gmail.com>
2020-01-08 03:52:19 +0100

    Movimiento sincronizado 100% real no fake, 1 link MEGA, HD 4K 1080p

--- Arcane-Royal/src/main/java/WebSocket/WebsocketHandler.java
+++ Arcane-Royal/src/main/java/WebSocket/WebsocketHandler.java
@@ -24,9 +24,8 @@ public class WebsocketHandler extends TextWebSocketHandler {
 				players++;
 				session.sendMessage(new TextMessage(""+players));
 			}else if(message.getPayload().equals("RONDA")){
-				mapas = "";
 				m = (int)(Math.random() * (5));
-				mapas += "" + m;
+				mapas = "" + m;
 				System.out.println("Mapa: "+ m);
 				for (int i=0; i < 4; i++) {
 					m = (int)(Math.random() * (5));			 
