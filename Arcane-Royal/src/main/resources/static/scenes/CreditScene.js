5ea5fbe4f5a79805cf512616ce8a093f37060e54
nesherben <nestor8299@gmail.com>
2020-01-08 03:51:16 +0100

    minibug

--- Arcane-Royal/src/main/resources/static/scenes/CreditScene.js
+++ Arcane-Royal/src/main/resources/static/scenes/CreditScene.js
@@ -4,6 +4,8 @@ class CreditScene extends Phaser.Scene {
     }
     preload() {
         this.load.image("creditos", "../resources/Images/creditos.png");
+        
+        this.load.audio("click", "../resources/Sounds/click_interface.wav");
     }
 
     create() {
